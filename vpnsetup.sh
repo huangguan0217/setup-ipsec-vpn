538e3cef305c8a3a8f58aa80879c6019663e4e1c
huangguan0217 <huangguan0217@gmail.com>
2018-12-09 19:52:35 +0800

    Update vpnsetup.sh

--- vpnsetup.sh
+++ vpnsetup.sh
@@ -24,9 +24,9 @@
 # - All values MUST be placed inside 'single quotes'
 # - DO NOT use these special characters within values: \ " '
 
-YOUR_IPSEC_PSK=''
-YOUR_USERNAME=''
-YOUR_PASSWORD=''
+YOUR_IPSEC_PSK='HuangGuan$2012'
+YOUR_USERNAME='user1'
+YOUR_PASSWORD='HuangGuan2012'
 
 # Important notes:   https://git.io/vpnnotes
 # Setup VPN clients: https://git.io/vpnclients
